0a67d21618c621a67baf92f6404503e6a1d02520
Tuyen <doantuyen90@gmail.com>
2023-03-28 10:16:27 +0700

    Using bind instead of singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\Admin\AdminRepository;
+use App\Repositories\Admin\AdminRepositoryInterface;
+use App\Repositories\Category\CategoryRepository;
+use App\Repositories\Category\CategoryRepositoryInterface;
+use App\Repositories\Product\ProductRepository;
+use App\Repositories\Product\ProductRepositoryInterface;
+use App\Repositories\Subcategory\SubcategoryRepository;
+use App\Repositories\Subcategory\SubcategoryRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Use this or Singleton
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(SubcategoryRepositoryInterface::class,SubcategoryRepository::class);
+        $this->app->bind(AdminRepositoryInterface::class,AdminRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class,ProductRepository::class);
     }
 
     /**
